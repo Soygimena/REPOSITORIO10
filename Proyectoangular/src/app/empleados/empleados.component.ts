1c2dfa92d793908564719a79680d49814f08af6e
Gimena <navarrogiim02@gmail.com>
2024-12-08 14:31:24 -0500

    agregar todos los archivos

---
+++ Proyectoangular/src/app/empleados/empleados.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+selector: "app-raiz",
+templateUrl: "./empleados.component.html",
+styleUrl: "./empleados.component.css"
+
+
+})
+export class EmpleadosComponent {
+    
+    carrera="DESARROLLO DE SISTEMAS DE INFORMACIÓN";
+    
+}
